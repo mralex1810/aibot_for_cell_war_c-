598610ea926fa252f119c6408c381f20f480c0cf
mralex1810 <sash39av@gmail.com>
2021-06-16 17:17:39 +0300

    Cell and Tentacle work

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <string>
-#include <vector> 
+#include <vector>
 #include <set>
 #include <algorithm>
 #include <math.h>
 #include <random>
 #include <ctime>
 
-
 using namespace std;
 
 int CELL_COUNTER;
-vector <vector<char>> cell_action(10, vector<char> (64));
-vector <vector<char>> tentacle_action(10, vector<char> (64));
-const vector <int> lvlscore = {0, 10, 25, 100, 250, 500, 990};
-int lvl_to_score(int score){
+vector<vector<unsigned char>> cell_action(10, vector<unsigned char>(64));
+vector<vector<unsigned char>> tentacle_action(10, vector<unsigned char>(64));
+const vector<int> lvlscore = {0, 10, 25, 100, 250, 500, 990};
+int lvl_to_score(int score)
+{
     return upper_bound(lvlscore.begin(), lvlscore.end(), score) - lvlscore.begin() - 1;
 }
-int distance(pair<int, int> pos1, pair<int, int> pos2){
-    return ((int) sqrt((pos1.first - pos2.first) * (pos1.first - pos2.first) +
-     (pos1.second - pos2.second) * (pos1.second - pos2.second))) / 10;
+int distance(pair<int, int> pos1, pair<int, int> pos2)
+{
+    return ((int)sqrt((pos1.first - pos2.first) * (pos1.first - pos2.first) +
+                      (pos1.second - pos2.second) * (pos1.second - pos2.second))) /
+           10;
 }
 struct Tentacle;
 struct Cell
@@ -27,10 +29,10 @@ struct Cell
     string owner;
     int score, counter, lvl, id;
     pair<int, int> pos;
-    vector <Tentacle*> tentacles;
+    vector<Tentacle *> tentacles;
 
-
-    Cell(string owner, int lvl, pair<int, int> pos){
+    Cell(string owner, int lvl, pair<int, int> pos)
+    {
         this->owner = owner;
         this->lvl = lvl;
         this->pos = pos;
@@ -40,82 +42,107 @@ struct Cell
         counter = 0;
     }
 
-    void attack(string attacker, int damage);
-    void update();
-    bool add_tentacle(Tentacle* tentacle);
-    void delete_tentacle(Tentacle* tentacle);
-};
-
-void Cell::delete_tentacle(Tentacle* tentacle){
-    for (int i = 0; i < tentacles.size(); i++){
-        if (tentacles[i] == tentacle){
-            swap(tentacles[i], tentacles[tentacles.size() - 1]);
-            tentacles.pop_back();
+    void Cell::delete_tentacle(Tentacle *tentacle)
+    {
+        for (int i = 0; i < tentacles.size(); i++)
+        {
+            if (tentacles[i] == tentacle)
+            {
+                swap(tentacles[i], tentacles[tentacles.size() - 1]);
+                tentacles.pop_back();
+            }
         }
     }
-}
+
+    void attack(string attacker, int damage);
+    void update();
+    bool add_tentacle(Tentacle *tentacle);
+    void delete_tentacle(Tentacle *tentacle);
+};
+
 struct Tentacle
 {
-    Cell* attacker;
-    Cell* attacked;
+    Cell *attacker;
+    Cell *attacked;
     int len;
     bool need_attack;
-    Tentacle(Cell* attacker, Cell* attacked){
+    Tentacle(Cell *attacker, Cell *attacked)
+    {
         this->attacker = attacker;
         this->attacked = attacked;
         len = distance(attacker->pos, attacked->pos);
         need_attack = false;
     }
-    void destroy(){
+    void destroy()
+    {
         attacker->attack(attacker->owner, len / 2);
         attacked->attack(attacker->owner, len / 2);
         attacker->delete_tentacle(this);
     }
-    void destroy_in_attacked(){
+    void destroy_in_attacked()
+    {
         attacked->attack(attacker->owner, len);
     }
-    void update(){
-        if (need_attack){
+    void update()
+    {
+        if (need_attack)
+        {
             need_attack = false;
             attacked->attack(attacker->owner, 1);
         }
     }
 };
-bool Cell::add_tentacle(Tentacle* tentacle){
-    if (tentacle->len <= score && tentacles.size() < lvl){
+bool Cell::add_tentacle(Tentacle *tentacle)
+{
+    if (tentacle->len <= score && tentacles.size() < lvl)
+    {
         tentacles.push_back(tentacle);
         score -= tentacle->len;
-
     }
 }
-void Cell::update(){
-    if (owner == "gray"){
+void Cell::update()
+{
+    if (owner == "gray")
+    {
         return;
     }
     counter += lvl + 1;
-    if (counter > 30){
+    if (counter > 30)
+    {
         counter = 0;
-        if (score < 1000) score++;
-        else counter += 10;
+        if (score < 1000)
+            score++;
+        else
+            counter += 10;
         lvl = lvl_to_score(score);
-        for (auto tentacle: tentacles)
+        for (auto tentacle : tentacles)
             tentacle->need_attack = true;
     }
 }
-void Cell::attack(string attacker, int damage){
-    if (owner == attacker){
-        if (score >= 1000){
+void Cell::attack(string attacker, int damage)
+{
+    if (owner == attacker)
+    {
+        if (score >= 1000)
+        {
             counter += 20;
-        } else {
+        }
+        else
+        {
             score += damage;
         }
         lvl = lvl_to_score(score);
-    } else if (damage <= score){
+    }
+    else if (damage <= score)
+    {
         score -= damage;
         if (owner != "gray")
             lvl = lvl_to_score(score);
-    } else{
-        for (auto tentacle : tentacles){
+    }
+    else
+    {
+        for (auto tentacle : tentacles)
+        {
             tentacle->destroy_in_attacked();
         }
         tentacles.clear();
@@ -128,36 +155,41 @@ void Cell::attack(string attacker, int damage){
     }
 }
 
-struct AI{
-    vector <double> gen;
+struct AI
+{
+    vector<double> gen;
     string owner;
 
-    AI(string owner, vector <double> gen){
+    AI(string owner, vector<double> gen)
+    {
         this->owner = owner;
         this->gen = gen;
     }
-
-
-
 };
 
-
-int main(){
+int main()
+{
     mt19937 gen32;
     gen32.seed(time(nullptr));
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 24; j++){
-            cell_action[i][j] = (char) gen32();
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 24; j++)
+        {
+            cell_action[i][j] = (char)gen32();
         }
     }
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 24; j++){
-            tentacle_action[i][j] = (char) gen32();
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 24; j++)
+        {
+            tentacle_action[i][j] = (char)gen32();
         }
     }
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 24; j++){
-            cout << (int) cell_action[i][j] << ' ';
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 24; j++)
+        {
+            cout << (int)cell_action[i][j] << ' ';
         }
         cout << endl;
     }
