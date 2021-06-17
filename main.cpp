dfb03da20eaf97bcffd7ed4facc0c2e1ff63211f
mralex1810 <78431758+mralex1810@users.noreply.github.com>
2021-06-17 14:03:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ const int GENERATIONS = 30;
 const int GENES = 64;
 const int CHANCE_MUTATION = 10;
 const int GEN_MUTATION = 40;
+const int CELLS_COUNT = 45;
 
-int CELL_COUNTER = 0;
 mt19937 gen32;
 vector<vector<char>> cell_action(10, vector<char>(64));
 vector<vector<char>> tentacle_action(10, vector<char>(64));
@@ -32,12 +32,17 @@ int distance(pair<int, int> pos1, pair<int, int> pos2)
                       (pos1.second - pos2.second) * (pos1.second - pos2.second))) /
            10;
 }
-struct Tentacle;
+class Tentacle;
 
-struct Cell
+class Cell
 {
+public:
+
     string owner;
-    int score, counter, lvl, id;
+    int score;
+    int lvl;
+    int id;
+    int counter;
     pair<int, int> pos;
     vector<Tentacle *> tentacles;
 
@@ -46,10 +51,10 @@ struct Cell
         this->owner = owner;
         this->lvl = lvl;
         this->pos = pos;
-        score = lvlscore[lvl];
+        this->score = lvlscore[lvl];
         tentacles = {};
-        id = CELL_COUNTER++;
         counter = 0;
+        id = CELL_COUNTER++;
     }
 
     void delete_tentacle(Tentacle *tentacle)
@@ -69,8 +74,9 @@ struct Cell
     bool add_tentacle(Tentacle *tentacle);
 };
 
-struct Tentacle
+class Tentacle
 {
+public:
     Cell *attacker;
     Cell *attacked;
     int len, id;
@@ -127,17 +133,22 @@ void Cell::update()
     {
         return;
     }
-    counter += lvl + 1;
+    int tmp = counter;
+    counter = tmp + lvl + 1;
+    
     if (counter >= 30)
     {
         counter = 0;
-        if (score < 1000)
+        if (score < 1000){
             score++;
-        else
-            counter += 10;
+        }
+        else{
+            counter += 20;
+        }
         lvl = lvl_to_score(score);
-        for (auto tentacle : tentacles)
+        for (auto tentacle : tentacles){
             tentacle->need_attack = true;
+        }
     }
 }
 void Cell::attack(string attacker, int damage)
@@ -182,18 +193,17 @@ void Tentacle::destroy()
     attacker->delete_tentacle(this);
     delete_tentacle(id, tentacles);
     (*tentacles_ways)[attacker->id][attacked->id] = false;
-    delete this;
 }
 void Tentacle::destroy_in_attacked()
 {
     attacked->attack(attacker->owner, len);
     delete_tentacle(id, tentacles);
     (*tentacles_ways)[attacker->id][attacked->id] = false;
-    delete this;
 }
 
-struct Chromosome
+class Chromosome
 {
+public:
     vector<char> chromosome;
     Chromosome()
     {
@@ -229,8 +239,9 @@ struct Chromosome
     }
 };
 
-struct AI
+class AI
 {
+public:
     Chromosome gens;
     string owner;
 
@@ -240,32 +251,32 @@ struct AI
         this->gens = gens;
     }
 
-    vector<pair<int, int>> process(vector<Cell> &cells, vector<vector<bool>> &tentacles_ways, int tick)
+    vector<pair<int, int>> process(vector<Cell*> &cells, vector<vector<bool>> &tentacles_ways, int tick)
     {
         vector<pair<int, int>> ans;
         for (auto cell_begin : cells)
         {
-            if (cell_begin.owner != owner)
+            if (cell_begin->owner != owner)
                 continue;
             for (auto cell_end : cells)
             {
-                if (cell_begin.id == cell_end.id)
+                if (cell_begin->id == cell_end->id)
                     continue;
                 bool is_enemy = 0;
                 bool is_gray = 0;
                 bool is_friend = 0;
-                if (cell_end.owner == "gray")
+                if (cell_end->owner == "gray")
                     is_gray = 1;
-                else if (cell_end.owner != cell_begin.owner)
+                else if (cell_end->owner != cell_begin->owner)
                     is_enemy = 1;
                 else
                     is_friend = 1;
-                int way = distance(cell_begin.pos, cell_end.pos);
-                if (!tentacles_ways[cell_begin.id][cell_end.id])
+                int way = distance(cell_begin->pos, cell_end->pos);
+                if (!tentacles_ways[cell_begin->id][cell_end->id])
                 {
                     vector<int> reasons_cell = {way / 20, 15000 / way, 100 * is_enemy, 100 * is_gray, 100 * is_friend,
-                                                cell_end.score / 10, cell_begin.score / 10, (cell_end.score - cell_begin.score) / 10,
-                                                tick / 50, cell_end.lvl * 100 / 7};
+                                                cell_end->score / 10, cell_begin->score / 10, (cell_end->score - cell_begin->score) / 10,
+                                                tick / 50, cell_end->lvl * 100 / 7};
                     int battle = 0;
                     for (int i = 0; i < 10; i++)
                     {
@@ -274,15 +285,15 @@ struct AI
                     }
                     if (battle > 0)
                     {
-                        ans.push_back(make_pair(cell_begin.id, cell_end.id));
+                        ans.push_back(make_pair(cell_begin->id, cell_end->id));
                         //cout << "attack\n";
                     }
                 }
                 else
                 {
                     vector<int> reasons_tentacle = {way / 20, 15000 / way, 100 * is_enemy, 100 * is_gray, 100 * is_friend,
-                                                    cell_end.score / 10, cell_begin.score / 10, (cell_end.score - way / 2) / 8,
-                                                    tick / 50, cell_end.lvl * 100 / 7};
+                                                    cell_end->score / 10, cell_begin->score / 10, (cell_end->score - way / 2) / 8,
+                                                    tick / 50, cell_end->lvl * 100 / 7};
                     int destroy = 0;
                     for (int i = 0; i < 10; i++)
                     {
@@ -292,7 +303,7 @@ struct AI
                     }
                     if (destroy > 0)
                         {
-                            ans.push_back(make_pair(cell_begin.id, cell_end.id));
+                            ans.push_back(make_pair(cell_begin->id, cell_end->id));
                             //cout << "destroy\n";
                         }
                 }
@@ -301,72 +312,69 @@ struct AI
         return ans;
     }
 };
-
-vector<Cell> start_cells = {
-    Cell("gray", 1, make_pair(100, 150), CELL_COUNTER),
-    Cell("gray", 1, make_pair(100, 350), CELL_COUNTER),
-    Cell("green", 2, make_pair(100, 500), CELL_COUNTER),
-    Cell("gray", 1, make_pair(100, 650), CELL_COUNTER),
-    Cell("gray", 1, make_pair(100, 850), CELL_COUNTER),
-
-    Cell("gray", 3, make_pair(300, 100), CELL_COUNTER),
-    Cell("gray", 2, make_pair(300, 300), CELL_COUNTER),
-    Cell("gray", 2, make_pair(300, 500), CELL_COUNTER),
-    Cell("gray", 2, make_pair(300, 700), CELL_COUNTER),
-    Cell("gray", 3, make_pair(300, 900), CELL_COUNTER),
-
-    Cell("gray", 4, make_pair(500, 150), CELL_COUNTER),
-    Cell("gray", 3, make_pair(500, 350), CELL_COUNTER),
-    Cell("gray", 3, make_pair(500, 500), CELL_COUNTER),
-    Cell("gray", 3, make_pair(500, 650), CELL_COUNTER),
-    Cell("gray", 4, make_pair(500, 850), CELL_COUNTER),
-
-    Cell("gray", 5, make_pair(700, 100), CELL_COUNTER),
-    Cell("gray", 4, make_pair(700, 300), CELL_COUNTER),
-    Cell("gray", 4, make_pair(700, 500), CELL_COUNTER),
-    Cell("gray", 4, make_pair(700, 700), CELL_COUNTER),
-    Cell("gray", 5, make_pair(700, 900), CELL_COUNTER),
-
-    Cell("gray", 6, make_pair(950, 50), CELL_COUNTER),
-    Cell("gray", 5, make_pair(950, 250), CELL_COUNTER),
-    Cell("gray", 4, make_pair(950, 500), CELL_COUNTER),
-    Cell("gray", 3, make_pair(950, 750), CELL_COUNTER),
-    Cell("gray", 4, make_pair(950, 950), CELL_COUNTER),
-
-    Cell("gray", 5, make_pair(1200, 100), CELL_COUNTER),
-    Cell("gray", 4, make_pair(1200, 300), CELL_COUNTER),
-    Cell("gray", 4, make_pair(1200, 500), CELL_COUNTER),
-    Cell("gray", 4, make_pair(1200, 700), CELL_COUNTER),
-    Cell("gray", 5, make_pair(1200, 900), CELL_COUNTER),
-
-    Cell("gray", 4, make_pair(1400, 150), CELL_COUNTER),
-    Cell("gray", 3, make_pair(1400, 350), CELL_COUNTER),
-    Cell("gray", 3, make_pair(1400, 500), CELL_COUNTER),
-    Cell("gray", 3, make_pair(1400, 650), CELL_COUNTER),
-    Cell("gray", 4, make_pair(1400, 850), CELL_COUNTER),
-
-    Cell("gray", 3, make_pair(1600, 100), CELL_COUNTER),
-    Cell("gray", 2, make_pair(1600, 300), CELL_COUNTER),
-    Cell("gray", 2, make_pair(1600, 500), CELL_COUNTER),
-    Cell("gray", 2, make_pair(1600, 700), CELL_COUNTER),
-    Cell("gray", 3, make_pair(1600, 900), CELL_COUNTER),
-
-    Cell("gray", 1, make_pair(1800, 150), CELL_COUNTER),
-    Cell("gray", 1, make_pair(1800, 350), CELL_COUNTER),
-    Cell("red", 2, make_pair(1800, 500), CELL_COUNTER),
-    Cell("gray", 1, make_pair(1800, 650), CELL_COUNTER),
-    Cell("gray", 1, make_pair(1800, 850), CELL_COUNTER)};
-
+vector<Cell*> start_cells(){
+    vector<Cell*> start_cells;
+    int CELL_COUNTER = 0;
+    start_cells.push_back(new Cell("gray", 1, make_pair(100, 150), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 1, make_pair(100, 350), CELL_COUNTER));
+    start_cells.push_back(new Cell("green", 2, make_pair(100, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 1, make_pair(100, 650), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 1, make_pair(100, 850), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(300, 100), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 2, make_pair(300, 300), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 2, make_pair(300, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 2, make_pair(300, 700), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(300, 900), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(500, 150), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(500, 350), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(500, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(500, 650), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(500, 850), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 5, make_pair(700, 100), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(700, 300), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(700, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(700, 700), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 5, make_pair(700, 900), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 6, make_pair(950, 50), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 5, make_pair(950, 250), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(950, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(950, 750), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(950, 950), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 5, make_pair(1200, 100), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(1200, 300), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(1200, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(1200, 700), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 5, make_pair(1200, 900), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(1400, 150), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(1400, 350), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(1400, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(1400, 650), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 4, make_pair(1400, 850), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(1600, 100), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 2, make_pair(1600, 300), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 2, make_pair(1600, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 2, make_pair(1600, 700), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 3, make_pair(1600, 900), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 1, make_pair(1800, 150), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 1, make_pair(1800, 350), CELL_COUNTER));
+    start_cells.push_back(new Cell("red", 2, make_pair(1800, 500), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 1, make_pair(1800, 650), CELL_COUNTER));
+    start_cells.push_back(new Cell("gray", 1, make_pair(1800, 850), CELL_COUNTER));
+    return start_cells;
+}
 void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, vector<pair<int, int>> &score)
 {
     int start_time = time(nullptr);
     int TENTACLE_COUNTER = 0;
-    vector<Cell> cells = start_cells;
+    vector<Cell*> cells;
+    for (auto cell : start_cells()){
+        cells.push_back(cell);
+    }
 
     AI greenbot = AI("green", green_gens);
     AI redbot = AI("red", red_gens);
     vector<Tentacle> tentacles;
-    vector<vector<bool>> tentacles_ways(CELL_COUNTER, vector<bool>(CELL_COUNTER, false));
+    vector<vector<bool>> tentacles_ways(CELLS_COUNT, vector<bool>(CELLS_COUNT, false));
     for (int tick = 0; tick < 5000; tick++)
     {
         vector<pair<int, int>> green_ans = greenbot.process(cells, tentacles_ways, tick);
@@ -376,7 +384,7 @@ void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, 
             {
                 for (auto tentacle : tentacles)
                 {
-                    if (tentacle.attacker->id == action.first and tentacle.attacked->id == action.second)
+                    if (tentacle.attacker->id == action.first && tentacle.attacked->id == action.second)
                     {
                         tentacle.destroy();
                         tentacles_ways[action.first][action.second] = false;
@@ -387,8 +395,8 @@ void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, 
             else
             {
 
-                Tentacle tentacle = Tentacle(&cells[action.first], &cells[action.second], TENTACLE_COUNTER, &tentacles_ways, &tentacles);
-                if (cells[action.first].add_tentacle(&tentacle))
+                Tentacle tentacle = Tentacle(cells[action.first], cells[action.second], TENTACLE_COUNTER, &tentacles_ways, &tentacles);
+                if (cells[action.first]->add_tentacle(&tentacle))
                 {
                     tentacles.push_back(tentacle);
                     tentacles_ways[action.first][action.second] = 1;
@@ -405,7 +413,7 @@ void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, 
             {
                 for (auto tentacle : tentacles)
                 {
-                    if (tentacle.attacker->id == action.first and tentacle.attacked->id == action.second)
+                    if (tentacle.attacker->id == action.first && tentacle.attacked->id == action.second)
                     {
                         tentacle.destroy();
                         tentacles_ways[action.first][action.second] = false;
@@ -416,8 +424,8 @@ void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, 
             else
             {
 
-                Tentacle tentacle = Tentacle(&cells[action.first], &cells[action.second], TENTACLE_COUNTER, &tentacles_ways, &tentacles);
-                if (cells[action.first].add_tentacle(&tentacle))
+                Tentacle tentacle = Tentacle(cells[action.first], cells[action.second], TENTACLE_COUNTER, &tentacles_ways, &tentacles);
+                if (cells[action.first]->add_tentacle(&tentacle))
                 {
                     tentacles.push_back(tentacle);
                     tentacles_ways[action.first][action.second] = 1;
@@ -428,7 +436,7 @@ void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, 
             }
         }
         for (auto cell : cells)
-            cell.update();
+            cell->update();
         for (auto tentacle : tentacles)
             tentacle.update();
     }
@@ -436,10 +444,13 @@ void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, 
     int red_score = 0;
     for (auto cell : cells)
     {
-        if (cell.owner == "green")
-            green_score += 500 + cell.score;
-        else if (cell.owner == "red")
-            red_score += 500 + cell.score;
+        if (cell->owner == "green"){
+            green_score += 500 + cell->score;
+            cout << cell->score << endl;
+        }else if (cell->owner == "red"){
+            red_score += 500 + cell->score;
+            cout << cell->score << endl;
+        }
     }
     cout << green_score << ' ' << red_score << ' ' << time(nullptr) - start_time << endl;
 }
@@ -447,7 +458,7 @@ void game(Chromosome &green_gens, Chromosome &red_gens, int green_i, int red_i, 
 int main()
 {
 
-    gen32.seed(1343000);
+    gen32.seed(time(nullptr));
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 24; j++)
@@ -466,4 +477,5 @@ int main()
     Chromosome gen2 = Chromosome();
     vector<pair<int, int>> v;
     game(gen1, gen2, 0, 0, v);
+    
 }
